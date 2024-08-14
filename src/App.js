5f096fe52278da01270262f3f7b8ff25d8fc05a1
purvisanghani02 <purvisanghani02@gmail.com>
2024-08-14 18:21:57 +0530

    add project on jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [data, setdata] = useState("");
+
+  const changehandler = () => {
+    setdata("heloooooo");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>helo</p>
+      <button data-testid="btn1" onClick={changehandler}>
+        update
+      </button>
+      <h1>{data}</h1>
+      <br />
+      <label htmlFor="example">Example</label>
+      <input id="example" type="text" defaultValue="hello" />
+      <br />
+      {/* multiple input type -------------------- */}
+      <button>click 1</button>
+      <button>click 2</button>
+      <br />
+      <label htmlFor="user">user name</label>
+      <input type="text" id="user" />
+      <div role="dummy">dummy data</div>
+
+      <br />
+
+      {/* getall by role---------------------------- */}
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <button>click me</button>
+      <br />
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
+      <br />
+      {/* checkbox---------- */}
+      <label htmlFor="skills">skill</label>
+      <input type="checkbox" id="skills" defaultChecked />
+      <br />
+      {/* getAllBylable--------------  */}
+      <label htmlFor="users1">User name list</label>
+      <input id="users1" type="text" defaultValue={"dipali"} />
+      <label htmlFor="users2">User name list</label>
+      <input id="users2" type="text" defaultValue={"dipali"} />
+      <label htmlFor="users3">User name list</label>
+      <input id="users3" type="text" defaultValue={"dipali"} />
+      <br />
+
+      {/* placeholder------------------ */}
+      <input type="text" placeholder="enter your name" />
+      <input type="text" placeholder="enter your name" />
+      <input type="text" placeholder="enter your name" />
+
+      {/* getByText-------------------- */}
+      <button>button</button>
+      <h1>hello react</h1>
+
+      <p className="paraStyle" id="p1 ">
+        p tag testing
+      </p>
+
+      {/* getByTestId------------------- */}
+      {/* <button data-testid="testid">click me</button> */}
+      {/* <h1 data-testid="testid">hello react</h1> */}
+      <h1 element-id="testid">hello react</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,114 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen, configure } from "@testing-library/react";
+import App from "./App.js";
 
-test('renders learn react link', () => {
+// configure({ testIdAttribute: "element-id" });
+
+test("text on screen", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/helo/i);
+  expect(text).toBeInTheDocument();
+});
+
+describe("UI test", () => {
+  test("text on screen", () => {
+    render(<App />);
+    const text = screen.getByText(/helo/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test("update", () => {
+    render(<App />);
+    const btn1 = screen.getByTestId("btn1");
+    const text = screen.getByText(/helo/i);
+    expect(text).toBeInTheDocument();
+    fireEvent.click(btn1);
+    expect(screen.getByText("heloooooo")).toBeInTheDocument();
+  });
+
+  test("input test", () => {
+    render(<App />);
+    const inputfeild = screen.getByRole("textbox", { name: "Example" });
+    const exampleInput = screen.getByLabelText("Example");
+    expect(exampleInput).toBeInTheDocument();
+    expect(inputfeild).toHaveValue("hello");
+  });
+
+  //multiple input type------------------
+  test("get-By-Role tesing", () => {
+    render(<App />);
+    const btn = screen.getByRole("button", { name: "click 1" });
+    const btn2 = screen.getByRole("button", { name: "click 2" });
+    const inputtype = screen.getByRole("textbox", { name: /user name/ });
+    const div = screen.getByRole("dummy");
+    expect(btn).toBeInTheDocument();
+    expect(btn2).toBeInTheDocument();
+    expect(inputtype).toBeInTheDocument();
+    expect(div).toBeInTheDocument();
+  });
+
+  //getAllByRole-------------------------------------
+  test("get-AllBy-Role tesing", () => {
+    render(<App />);
+    //multiple button----------
+    const btn = screen.getAllByRole("button");
+    for (let i = 0; i < btn.length; i++) {
+      expect(btn[i]).toBeInTheDocument();
+    }
+    //select option------------
+    const options = screen.getAllByRole("option");
+    for (let i = 0; i < options.length; i++) {
+      expect(options[i]).toBeInTheDocument();
+    }
+  });
+
+  //checkobx-----------------------------------------
+  test("checkobx testing", () => {
+    render(<App />);
+    const checkobx = screen.getByLabelText("skill");
+    expect(checkobx).toBeInTheDocument();
+    expect(checkobx).toBeChecked();
+  });
+
+  // getAllBylable--------------------------------
+  test("get-AllBy-lable tesing", () => {
+    render(<App />);
+    const lable = screen.getAllByLabelText("User name list");
+    for (let i = 0; i < lable.length; i++) {
+      expect(lable[i]).toBeInTheDocument();
+      expect(lable[i]).toHaveValue("dipali");
+    }
+  });
+
+  // placeholder--------------------------------
+  test("placeholder testing", () => {
+    render(<App />);
+    const placeholder = screen.getAllByPlaceholderText("enter your name");
+
+    for (let i = 0; i < placeholder.length; i++) {
+      expect(placeholder[i]).toBeInTheDocument();
+    }
+  });
+
+  //getByText------------------------------------
+  test("get-AllBy-Text testing", () => {
+    render(<App />);
+    const button = screen.getByText("button");
+    expect(button).toBeInTheDocument();
+    const ptag = screen.getByText("p tag testing");
+    expect(ptag).toBeInTheDocument();
+    expect(ptag).toHaveClass("paraStyle");
+    expect(ptag).toHaveAttribute("id");
+  });
+
+  //getByTestId------------------------------------
+  test("get-AllBy-TestId testing", () => {
+    render(<App />);
+    const button = screen.getAllByTestId("testid");
+    for (let i = 0; i < button.length; i++) {
+      expect(button[i]).toBeInTheDocument();
+    }
+
+    //to override data-testId in testId use configure
+    // const h1 = screen.getByTestId("testid");
+  });
 });
